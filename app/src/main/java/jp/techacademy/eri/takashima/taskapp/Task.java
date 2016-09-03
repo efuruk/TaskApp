fc0737d348d13a7a2a44b821da165f2f4d171ab6
Eri Takashima <efuruk@gmail.com>
2016-09-03 07:52:17 -0500

    Gradleの変更

---
+++ app/src/main/java/jp/techacademy/eri/takashima/taskapp/Task.java
@@ -0,0 +1,45 @@
+package jp.techacademy.eri.takashima.taskapp;
+
+/**
+ * Created by hajime04 on 9/3/2016.
+ */
+
+import java.io.Serializable;
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realmannotations.PrimaryKey;
+
+public class Task extends RealmObject implements Serializable {
+    private String title;
+    private String contents;
+    private Date date;
+
+    @PrimaryKey
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContents() {
+        return contents;
+    }
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+}
